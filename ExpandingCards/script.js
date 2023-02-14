a3158b73043c79e718404da7c3624c5be3c9189b
betulturkut <btlturkut@gmail.com>
2023-02-15 00:30:29 +0300

    Update script.js

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -2,7 +2,6 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach(panel => {
     panel.addEventListener("click", () => {
-
         removeActive();
         panel.classList.add("active");
     });
